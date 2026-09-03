23443bbab44295f012f5ddab13942a7426ef7be2
gidedr44 <gidedr44@gmail.com>
2026-09-02 23:32:59 -0400

    Rename project to locallibrary_config and fix bugs

--- locallibrary_config/settings.py
+++ locallibrary_config/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'locallibrary.urls'
+ROOT_URLCONF = 'locallibrary_config.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'locallibrary.wsgi.application'
+WSGI_APPLICATION = 'locallibrary_config.wsgi.application'
 
 
 # Database
