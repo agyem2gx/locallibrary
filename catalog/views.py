8b2ececeab4525de526a7569aa79168fbe0c2a8c
gidedr44 <gidedr44@gmail.com>
2026-09-16 15:07:45 -0400

    Complete Part 5 challenges: title, extra counts, external and static images

--- catalog/views.py
+++ catalog/views.py
@@ -5,22 +5,22 @@ from .models import Book, Author, BookInstance, Genre
 def index(request):
     """View function for home page of site."""
 
-    # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
-
-    # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
-
-    # The 'all()' is implied by default.
     num_authors = Author.objects.count()
+
+    # Challenge: counts for genres and books containing a word
+    num_genres = Genre.objects.all().count()
+    num_books_with_fiction = Book.objects.filter(title__icontains='fiction').count()
 
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_books_with_fiction': num_books_with_fiction,
     }
 
-    # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
